c76f42ecae7858018de84f73a0e3b14844dec3fa
Tim Aerts <aardbeiplantje@gmail.com>
2026-05-19 22:08:47 +0200

    Revert "ipv6"
    This reverts commit b1b292a75bbf5544ac3531bb66b9d01de97db8ad.

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,6 @@ export STACK_NAME=${STACK_NAME:-$APP_NAME}
 printf -v now "%(%s)T" -1
 export CFG_PREFIX=$STACK_NAME-$now
 export TC_HTB_RATE=${TC_HTB_RATE:-50Mbit}
-ipv6_prefix=${APP_IPV6_PREFIX?Need APP_IPV6_PREFIX}
-export IPV6=${IPV6?Need IPV6}
 
 cd $WORKSPACE || exit $?
 
@@ -45,41 +43,6 @@ fi
 
 echo "building images with buildx bake, using $DOCKER_REPOSITORY"
 docker buildx bake -f docker-bake.hcl || exit $?
-
-echo "running sysctl for proxy_ndp and add $IPV6 as neighbour"
-APP_IF_NAME=${APP_IF_NAME:-eno1}
-sudo sh -x -c "ip -6 neigh add proxy $IPV6 dev $APP_IF_NAME; \
-    sysctl net.ipv6.conf.default.proxy_ndp=1; \
-    sysctl net.ipv6.conf.all.proxy_ndp=1; \
-    ip6tables -D DOCKER -s ::/0 -d $IPV6 -p tcp --dport 8080 -j ACCEPT; \
-    ip6tables -I DOCKER -s ::/0 -d $IPV6 -p tcp --dport 8080 -j ACCEPT"
-
-echo "creating network $nw_name with prefix $ipv6_prefix"
-if_name=${BRIDGE_IF_NAME:-dmz-${STACK_NAME}0}
-nw_name=dmz-$STACK_NAME
-docker stack rm $STACK_NAME
-sleep 3
-docker network rm $nw_name || true
-sleep 3
-docker network create \
-    $nw_name \
-    --ipv6 \
-    --ipv4 \
-    --attachable \
-    --scope=swarm \
-    --subnet=$ipv6_prefix \
-    --driver=bridge \
-    -o com.docker.network.bridge.name=$if_name \
-    -o com.docker.network.container_iface_prefix=dmz \
-    -o com.docker.network.bridge.gateway_mode_ipv6=routed \
-    -o com.docker.network.bridge.enable_icc=true \
-    -o com.docker.network.bridge.enable_ip_masquerade=false \
-    -o com.docker.network.bridge.enable_ip6_masquerade=false \
-    -o com.docker.network.enable_ipv6=true \
-    -o com.docker.network.bridge.inhibit_ipv4=true \
-    -o com.docker.network.driver.mtu=1500 \
-    --ipam-driver default \
-    || exit $?
 
 echo "stack deploy $STACK_NAME, using $DOCKER_REPOSITORY"
 docker stack deploy \
